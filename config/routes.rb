433811eeb82b423a9d9a912b56861766501f3eb4
teshio0512 <teshio0512@gmail.com>
2024-09-08 09:57:57 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :messages, only: :index
-  resources :rooms, only: [:new, :delete]
+  resources :rooms, only: [:new, :create]
   resources :users, only: [:edit, :update]
 end
